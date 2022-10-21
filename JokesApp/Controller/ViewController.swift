fa7879acdb8a5898b289ba2c54d0ba8aae4d1d40
Varun Bhola <vbhola@aapnainfotech.com>
2022-10-21 20:41:37 +0530

    kdhg

--- JokesApp/Controller/ViewController.swift
+++ JokesApp/Controller/ViewController.swift
@@ -12,6 +12,23 @@ final class ViewController: UIViewController {
     var jokesViewModel = JokesViewModel()
     private lazy var imageContainerView: UIView = {
        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    lazy var barLabel: UILabel = {
+        let label = UILabel()
+       // label.font = .textStyle9
+        label.textColor = .white
+        label.text = "Jokes"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    private lazy var tabBar: UIView = {
+       let view = UIView()
         view.backgroundColor = .red
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -21,21 +38,55 @@ final class ViewController: UIViewController {
         
         return table
     }()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        jokesTable.estimatedRowHeight = 100
+//        jokesTable.rowHeight = UITableView.automaticDimension
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        jokesViewModel.getAllJokesData()
+//        DispatchQueue.global().asyncAfter(qos: .background).async {
+//            // self.BackgroundActivity()
+//            self.jokesViewModel.getAllJokesData()
+//            DispatchQueue.main.async {
+//                print("Hello")
+//                self.jokesTable.reloadData()
+//            }
+//        }
+        Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+      
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+//
+//        }
         jokesTable.delegate = self
         jokesTable.dataSource = self
         jokesTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(self.tabBar)
+        self.tabBar.addSubview(self.barLabel)
         self.view.addSubview(self.imageContainerView)
         self.imageContainerView.addSubview(jokesTable)
         setupLayout()
         // Do any additional setup after loading the view.
     }
-
+    @objc func update() {
+        self.jokesViewModel.getAllJokesData()
+        self.jokesTable.reloadData()
+    }
     private func setupLayout(){
+        self.tabBar.snp.makeConstraints { make in
+            make.topMargin.equalTo(10)
+            make.leading.equalTo(0)
+            make.trailing.equalTo(0)
+            make.height.equalTo(50)
+        }
+        self.barLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalTo(35)
+        }
         self.imageContainerView.snp.makeConstraints { make in
-            make.leading.trailing.top.width.equalToSuperview()
+            make.top.equalTo(self.tabBar.snp.bottom)
+            make.leading.trailing.width.equalToSuperview()
             make.height.equalToSuperview()
         }
         
@@ -48,17 +99,30 @@ final class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return jokesViewModel.arrUser.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Jokes"
+        let model = jokesViewModel.arrUser[indexPath.row]
+        cell.textLabel?.text = model
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+//        if indexPath.section == 0 {
+//                return UITableView.automaticDimension
+//            } else {
+                return 100
+   //         }
+        //return UITableView.automaticDimension
     }
-    
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        if indexPath.section == 0 {
+//            return UITableView.automaticDimension
+//        } else {
+//            return 40
+//        }
+//    }
     
 }
--- JokesApp/ViewModel/JokesViewModel.swift
+++ JokesApp/ViewModel/JokesViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+
+
+class JokesViewModel{
+    
+    var arrUser = [String]()
+    weak var vc: ViewController?
+    func getAllJokesData(){
+        URLSession.shared.dataTask(with: URL(string: "https://geek-jokes.sameerkumar.website/api")!) { data, response, error in
+            if error == nil{
+                if let data = data{
+                    do{
+                        let userResponse = try JSONDecoder().decode(String.self, from: data)
+                        print(userResponse)
+                        if self.arrUser.count == 10{
+                            self.arrUser.remove(at: 0)
+                            self.arrUser.append(userResponse)
+                        }else{
+                          self.arrUser.append(userResponse)
+                        }
+                    }catch let err{
+                        print(err.localizedDescription)
+                    }
+                }
+            }else{
+                error?.localizedDescription
+            }
+        }.resume()
+    }
+
+}
+
+
+
